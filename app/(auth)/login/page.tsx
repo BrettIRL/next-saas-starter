337e8a8ef44753607a7b213e87b2638e7d116055
Brett Cocking <brett@trademarktechnology.co.za>
2023-09-20 14:37:01 +0200

    Change auth page metadata

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,8 +5,8 @@ import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = {
-  title: 'Authentication',
-  description: 'Authentication forms built using the components.',
+  title: 'Log In',
+  description: 'Log in to your account',
 };
 
 export default function LoginPage() {
@@ -19,7 +19,7 @@ export default function LoginPage() {
           'absolute right-4 top-4 md:right-8 md:top-8',
         )}
       >
-        Signup
+        Sign up
       </Link>
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900" />
@@ -53,10 +53,10 @@ export default function LoginPage() {
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
-              Create an account
+              Log in to account
             </h1>
             <p className="text-sm text-muted-foreground">
-              Enter your email below to create your account
+              Enter your email below to sign in your account
             </p>
           </div>
           <LoginForm />
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -5,8 +5,8 @@ import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = {
-  title: 'Authentication',
-  description: 'Authentication forms built using the components.',
+  title: 'Sign Up',
+  description: 'Create an account',
 };
 
 export default function SignupPage() {
@@ -19,7 +19,7 @@ export default function SignupPage() {
           'absolute right-4 top-4 md:right-8 md:top-8',
         )}
       >
-        Login
+        Log in
       </Link>
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900" />
